088799be341fec1029b898e5eab099a159171c87
Bohdan Starosta <bjstarosta@gmail.com>
2018-11-14 01:25:27 +0000

    var name update

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
-from displot.displot import displot_info
+from displot.displot import DISPLOT_INFO
 
 
 with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
-   name=displot_info['appTitle'],
-   version=displot_info['appVersion'],
+   name=DISPLOT_INFO['appTitle'],
+   version=DISPLOT_INFO['appVersion'],
    description='Structural dislocation detector.',
    long_description=long_description,
    license="GPLv3",
-   author=displot_info['author'],
-   author_email=displot_info['authorEmail'],
+   author=DISPLOT_INFO['author'],
+   author_email=DISPLOT_INFO['authorEmail'],
    packages=find_packages(),
    install_requires=[
     'numpy',
